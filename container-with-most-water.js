fc5d04915a193221cd1955f16b40bf0fcef7c7ad
NadyaCodes <nadyacorscadden@gmail.com>
2024-03-30 21:32:35 -0400

    add container with most water

---
+++ container-with-most-water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    const currentArea = Math.min(height[left], height[right]) * (right - left);
+    maxArea = Math.max(maxArea, currentArea);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea;
+};
